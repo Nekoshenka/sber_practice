485089966839bfe19c64b24dd75dcb5286fc4eaf
Nekosha <elderman1000@mail.ru>
2024-02-25 23:04:54 +0300

    All Task done

---
+++ Base/TaskOne/src/Task.java
@@ -0,0 +1,13 @@
+public class Task {
+
+    public static void main(String[] args) {
+
+        int cycle = 0;
+        while (cycle < 98) {
+            cycle += 7;
+            System.out.print(cycle + " ");
+        }
+
+    }
+
+}
